d5fe9e0ee61617a74ff315ba6da228fe4cc8f32b
emohrtuxedo <e.mohr@tuxedocomputers.com>
2020-09-10 13:52:24 +0200

    added testscript for variable type change

---
+++ test/typechange.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use strict qw(vars subs);
+use warnings;
+
+use Data::Dumper qw(Dumper);
+
+my %firstHash = (
+	module1 => {
+		aString => '1',
+		aNumber => 1,
+	},
+	module2 => {
+		aString => '2',
+		aNumber => 2,
+	},
+);
+
+
