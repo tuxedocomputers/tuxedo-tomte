5445e04cabe17a02e45e3ea232556a7ee8372735
E. Mohr <tux@tuxedocomputers.com>
2020-10-20 17:02:36 +0200

    added debian directory

--- pack1.sh
+++ pack1.sh
@@ -1,4 +1,6 @@
-@echo "!modify version number before using this!"
+!#/bin/sh
+
+echo "!modify version number before using this!"
 
 DEBFULLNAME="E. Mohr"
 DEBEMAIL="tux@tuxedocomputers.com"
@@ -7,8 +9,9 @@ VERSION=2.0.5
 PREFIX=${PACKAGE}_${VERSION}
 cd debian
 debchange -v ${VERSION}
+cd ..
 git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
-tar tvf ../${PREFIX}_${VERSION}.orig.tar.gz
+tar tvf ../${PREFIX}.orig.tar.gz
 cd ..
 git add .
 git commit -m 'building package'
