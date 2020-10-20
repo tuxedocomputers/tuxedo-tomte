7d08ebb36cd245031e1cdc9c979cdb3fa12634f1
E. Mohr <tux@tuxedocomputers.com>
2020-10-20 16:53:03 +0200

    building package

--- pack1.sh
+++ pack1.sh
@@ -1,17 +1,15 @@
-all:
-	@echo "!modify version number before using this!"
+@echo "!modify version number before using this!"
 
-pre:
-	DEBFULLNAME="E. Mohr"
-	DEBEMAIL="tux@tuxedocomputers.com"
-	PACKAGE=tuxedo-tomte
-	VERSION=2.0.5
-	PREFIX=${PACKAGE}_${VERSION}
-	cd debian
-	debchange -v ${VERSION}
-	git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
-	tar tvf ../${PREFIX}_${VERSION}.orig.tar.gz
-	cd ..
-	git add .
-	git commit -m 'building package'
-	git push
+DEBFULLNAME="E. Mohr"
+DEBEMAIL="tux@tuxedocomputers.com"
+PACKAGE=tuxedo-tomte
+VERSION=2.0.5
+PREFIX=${PACKAGE}_${VERSION}
+cd debian
+debchange -v ${VERSION}
+git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
+tar tvf ../${PREFIX}_${VERSION}.orig.tar.gz
+cd ..
+git add .
+git commit -m 'building package'
+git push
