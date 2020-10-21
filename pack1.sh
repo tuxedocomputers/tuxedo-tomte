bcf53a563efbc35ee6541fcf6351442083a6158e
E. Mohr <tux@tuxedocomputers.com>
2020-10-21 14:59:41 +0200

    packaging

--- pack1.sh
+++ pack1.sh
@@ -5,14 +5,13 @@ echo "!modify version number before using this!"
 DEBFULLNAME="E. Mohr"
 DEBEMAIL="tux@tuxedocomputers.com"
 PACKAGE=tuxedo-tomte
-VERSION=2.0.5
+VERSION=2.0.6
 PREFIX=${PACKAGE}_${VERSION}
 cd debian
 debchange -v ${VERSION}
 cd ..
 git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
 tar tvf ../${PREFIX}.orig.tar.gz
-cd ..
 git add .
 git commit -m 'building package'
 git push
