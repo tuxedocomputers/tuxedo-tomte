56a6ff8c0275aa3aaf99f90cb583d5fef65597e3
E. Mohr <tux@tuxedocomputers.com>
2020-10-30 13:45:55 +0100

    modified helper script

--- pack1.sh
+++ pack1.sh
@@ -11,8 +11,11 @@ export PREFIX=${PACKAGE}_${VERSION}
 cd debian
 debchange -v ${VERSION}
 cd ..
+echo "build tarball"
 git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
+echo "check what is inside tarball"
 tar tvf ../${PREFIX}.orig.tar.gz
+echo "commit"
 git add .
 git commit -m 'building package'
 git push
