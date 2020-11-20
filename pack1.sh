0b7a568745b2121dc456e247919ecb064177151a
E. Mohr <tux@tuxedocomputers.com>
2020-11-20 12:16:26 +0100

    building package

--- pack1.sh
+++ pack1.sh
@@ -19,3 +19,20 @@ echo "commit"
 git add .
 git commit -m 'building package'
 git push
+git checkout --orphan debian-upstream
+git rm --cached -r .
+git clean -xfd
+git commit --allow-empty -m 'Start of debian branches.'
+git checkout -b debian-debian
+git checkout preliminary -- debian/
+git add .
+git commit -m 'packing'
+touch debian/gbp.conf
+echo "[DEFAULT]" >> debian/gbp.conf
+echo "upstream-branch=debian-upstream" >> debian/gbp.conf
+echo "debian-branch=debian-debian" >> debian/gbp.conf
+git add .
+git commit -m 'packing'
+gbp import-orig --no-interactive ../${PACKAGE}_${VERSION}.orig.tar.gz
+gbp buildpackage -us -uc
+
