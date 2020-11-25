8099969c3d8ca96aef08f8510807a7024a36cee7
E. Mohr <tux@tuxedocomputers.com>
2020-11-25 14:34:19 +0100

    added changelog

--- pack-release.sh
+++ pack-release.sh
@@ -18,47 +18,47 @@ git commit -m 'added changelog'
 git push
 
 echo "##########################################"
-echo "#build tarball (git archive)"
+echo "# build tarball (git archive)"
 git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip -c > ../${PREFIX}.orig.tar.gz
-echo "#check what is inside tarball"
+echo "# check what is inside tarball"
 tar tvf ../${PREFIX}.orig.tar.gz
-echo "#commit"
+echo "# commit"
 git add .
 git commit -m 'building package'
 git push
 
 echo "##########################################"
-echo "#git checkout --orphan debian-upstream"
+echo "# git checkout --orphan debian-upstream"
 git checkout --orphan debian-upstream
-echo "#git rm --cached -r ."
+echo "# git rm --cached -r ."
 git rm --cached -r .
-echo "#it clean -xfd"
+echo "# it clean -xfd"
 git clean -xfd
-echo "#git commit --allow-empty -m 'Start of debian branches.'"
+echo "# git commit --allow-empty -m 'Start of debian branches.'"
 git commit --allow-empty -m 'Start of debian branches.'
-echo "#git checkout -b debian-debian"
+echo "# git checkout -b debian-debian"
 git checkout -b debian-debian
-echo "#git checkout preliminary -- debian/"
+echo "# git checkout preliminary -- debian/"
 git checkout preliminary -- debian/
-echo "#commit"
+echo "# commit"
 git add .
 git commit -m 'packing'
 
-echo "#create gdp.conf"
+echo "# create gdp.conf"
 touch debian/gbp.conf
 echo "[DEFAULT]" >> debian/gbp.conf
 echo "upstream-branch=debian-upstream" >> debian/gbp.conf
 echo "debian-branch=debian-debian" >> debian/gbp.conf
 
-echo "#commit"
+echo "# commit"
 git add .
 git commit -m 'packing'
 
 echo "##########################################"
-echo "#gbp import-orig --no-interactive ../${PACKAGE}_${VERSION}.orig.tar.gz"
+echo "# gbp import-orig --no-interactive ../${PACKAGE}_${VERSION}.orig.tar.gz"
 gbp import-orig --no-interactive ../${PACKAGE}_${VERSION}.orig.tar.gz
 
 echo "##########################################"
-echo "#gbp buildpackage -us -uc"
+echo "# gbp buildpackage -us -uc"
 gbp buildpackage -us -uc --git-color=on
 
