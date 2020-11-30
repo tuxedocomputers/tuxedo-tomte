0c6a8a00c7ef685ecf84b1e61723b3467d7e0fdc
E. Mohr <tux@tuxedocomputers.com>
2020-11-30 14:32:46 +0100

    read repo files implemented

--- test/read-dirs.pl
+++ test/read-dirs.pl
@@ -9,10 +9,38 @@ my $sourcesListDir = '/etc/apt/sources.list.d/';
 my %dirHash;
 my $DH;
 
+
+sub readFileReturnLines {
+    my $file = shift;
+    my $FH;
+    my @lines;
+    if ( open $FH, "<", $file ) {
+        @lines = <$FH>;
+        close $FH;
+    } else {
+        printLog("no $file present or unable to open the file for reading");
+        return;
+    }
+    return @lines;
+}
+
+
+
+
+my $fileName = '/etc/apt/sources.list';
+
+$dirHash{'/etc/apt/sources.list'} = [readFileReturnLines($fileName)];
+
 if (opendir($DH,$sourcesListDir)) {
 	while (readdir $DH) {
 		if ($_ eq '.' or $_ eq '..') { next; }
-		print "dir: $_\n";
+		$fileName = $sourcesListDir.$_;
+		$dirHash{$fileName} = [readFileReturnLines($fileName)];
 	}
 }
+closedir($DH);
+
+print Dumper(%dirHash);
+
+
 
