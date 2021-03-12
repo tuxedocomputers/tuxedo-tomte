cefdb2ba1f7c5c12979af85644fd96def78dcecf
E. Mohr <tux@tuxedocomputers.com>
2021-03-12 15:17:56 +0100

    added findPciDevices and rewrote nvme and micfix1 pci device query

--- test/lspci.pl
+++ test/lspci.pl
@@ -5,15 +5,25 @@ use warnings;
 use Data::Dumper qw(Dumper);
 
 my $lspciValues = `lspci -vn`;
-my @lspciLines = split("\n", $lspciValues);
 
-my @subsystemValues = ('1558:50f0', '1558:96e1');
-my $lspciLine;
-foreach $lspciLine (@lspciLines) {
-	if ($lspciLine =~ /Subsystem/) {
-		$lspciLine =~ /([0-9a-fA-F]{4}:[0-9a-fA-F]{4})/;
-		$lspciLine = $1;
-		print "$lspciLine\n";
-		foreach ()
-	}
+my @subsystemValues = ('1558:50f0', '1558:96e1', '8086:001[0-9A-Za-z]');
+my $lspciDevices = `lspci -vn`;
+
+sub findPciDevice {
+    my ($lspciList, @pciDevices) = @_;
+    my $pciDevice;
+    foreach $pciDevice (@pciDevices) {
+		$pciDevice = $pciDevice;
+		print "$pciDevice\n";
+        if ($lspciList =~ /$pciDevice/) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+if (findPciDevice($lspciDevices, @subsystemValues)) {
+	print "found\n";
+} else {
+	print "none\n";
 }
