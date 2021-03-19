8ddd4f1bea873f95e89fecc885852dc970c0e7e6
E. Mohr <tux@tuxedocomputers.com>
2021-03-19 11:01:27 +0100

    testscript for md5sum

--- test/md5sum.pl
+++ test/md5sum.pl
@@ -6,12 +6,56 @@ use Data::Dumper qw(Dumper);
 use Digest::MD5 qw(md5_hex);
 
 my $files;
+my $file;
+my $md5sum;
+my $md5Store = '/home/pablo/tuxedo-tomte/test/md5sums';
+my $line;
+my $noChange = 1;
 
+my %initrdHashFiles;
+my %initrdHashSaved;
 
-my $var = 123;
+if (opendir(BOOTDIR, '/boot')) {
+	while (readdir BOOTDIR) {
+		if ($_ =~ /initrd\.img/) {
+			local $/ = undef;
+			print ("file: $_ ");
+			if (open( INITRDFILE, "/boot/$_" ) ) {
+				binmode(INITRDFILE);
+				my $initrdData = <INITRDFILE>;
+				close INITRDFILE;
+				$md5sum = md5_hex($initrdData);
+				$initrdHashFiles{$_} = $md5sum;
+				print "hash: $md5sum\n";
+				close(INITRDFILE);
+			}
+		}
+	}
+	close(BOOTDIR);
+	my $FH;
+	if (open($FH, ">", $md5Store)) {
+		foreach $file (keys %initrdHashFiles) {
+			print "$file:$initrdHashFiles{$file}\n";
+			print $FH "$file: $initrdHashFiles{$file}\n";
+		}
+		close($FH);
+	}
+	if (open($FH, "<", $md5Store)) {
+		while ($line = <$FH>) {
+			chop($line);
+			($file, $md5sum) = split /:/, $line;
+			print "file: $file md5: $md5sum\n";
+			$initrdHashSaved{$file} = $md5sum;
+		}
+	}
+	foreach $file (keys %initrdHashFiles) {
+		if ($initrdHashFiles{$file} ne $initrdHashSaved{$file}) {
+			$noChange = 0;
+			last;
+		}
+	}
+}
 
-my $md5_hash = md5_hex($var);
-
-print $md5_hash;
+print "result: $noChange\n";
 
 
