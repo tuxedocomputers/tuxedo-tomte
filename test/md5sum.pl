4e8a34a8c56888c94cc16c51b559ed8edcdb7606
E. Mohr <tux@tuxedocomputers.com>
2021-03-23 15:46:05 +0100

    more md5 stuff

--- test/md5sum.pl
+++ test/md5sum.pl
@@ -6,11 +6,13 @@ use Data::Dumper qw(Dumper);
 use Digest::MD5 qw(md5_hex);
 
 my $configDir = '/home/pablo/tuxedo-tomte/test/';
+my %md5StoreHash;
+my $md5Store = $configDir.'md5sums';
+my $md5FilesHash;
 
 sub initrdChanged {
 	my $initrdFile;
 	my $md5sum;
-	my $md5Store = $configDir.'md5sums';
 	my $md5Line;
 	my $noChange = 1;
 
@@ -55,6 +57,51 @@ sub initrdChanged {
 		}
 	}
 	print "result: $noChange\n";
+}
+
+sub loadMD5Store {
+	my $md5line;
+	if (open(FH, "<", $md5Store)) {
+		while ($md5Line = <FH>) {
+			chop($md5Line);
+			my ($key, $val) = split /:/, $md5Line;
+			print "key: $key val: $val\n";
+			$md5StoreHash{$key} = $val;
+		}
+		close FH;
+		return 1;
+	} else {
+		print "no redable md5sum file available\n";
+		return 0;
+	}
+}
+
+sub readInitrdMD5Files {
+	my $intirdData;
+	my $md5sum;
+	if (opendir(BOOTDIR, '/boot')) {
+		while (readdir BOOTDIR) {
+			if ($_ =~ /initrd\.img/) {
+				local $/ = undef;
+				if (open( INITRDFILE, "/boot/$_" ) ) {
+					binmode(INITRDFILE);
+					$initrdData = <INITRDFILE>;
+					$md5sum = md5_hex($initrdData);
+					$md5FilesHash{$_} = $md5sum;
+					close(INITRDFILE);
+				}
+			}
+		}
+		close BOOTDIR;
+	}
+}
+
+sub updateMD5Store {
+	my @files = @_;
+}
+
+sub compareMD5 {
+	my @files = @_;
 }
 
 
