d21ce54acaa198f47b873ad629f02c4fab689cc9
M. Arnold <marnold@tuxedocomputers.com>
2024-02-19 15:44:02 +0100

    Version Release 2.24.0

--- release_commit.sh
+++ release_commit.sh
@@ -5,30 +5,32 @@ echo "Check git status"
 git status
 
 # check
-echo "\nCheck for consitency"
+echo "Check for consitency"
 ./check.sh
 
-echo "\nDo you wish to proceed based on these informations? (press enter to continue)";
+echo "Do you wish to proceed based on these informations? (press enter to continue)";
 read returnValue #proceeds with enter
 
 # Prompt user for the version
+VERSIONSTRING=$(dpkg-parsechangelog | awk '/^Version:/ {print $2}')
+echo "Last version is $VERSIONSTRING."
 echo "Please enter the version in format (X.X.X): "
 read version_tag
 commit_message="Version Release $version_tag"
 
 # Set default email if environment variable is not given
-if [{ -z "$EMAIL" || -z "$DEBEMAIL"}]; then
-  export DEBEMAIL="tux@tuxedocomputers.com";
+if [[ -z "$EMAIL" || -z "$DEBEMAIL" ]]; then
+  export DEBEMAIL="tux@tuxedocomputers.com"
 fi
 
 dch --newversion $version_tag $commit_message
 vim debian/changelog
+dch --release
 cp debian/changelog changelog
 
 # Set version tag and perform commit
-git tag $version_tag
-
 git add debian/changelog
 git add changelog
 
 git commit -m "$commit_message"
+git tag $version_tag
