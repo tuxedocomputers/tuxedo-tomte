e49aab7f99b250f1885dcab50a02a8bd8c92a7b1
9r3g0r <kisakuru@gmail.com>
2024-05-22 21:34:01 +0200

    cleaned up check_translations

--- check_translations.pl
+++ check_translations.pl
@@ -13,151 +13,153 @@ use TOML qw(from_toml to_toml);
 my %msgids;
 
 # Specify the directory to start the search
-my $script_directory = $RealBin;
-my $start_directory = $script_directory . '/po/';
+my $scriptDirectory = $RealBin;
+my startDirectory = $scriptDirectory . '/po/';
 my $defaultLanguage = 'en';
 
 
 # Define the subroutine to be executed for each file
-sub load_msg_ids_from_directories {
-    my $file_path = $File::Find::name;
+sub loadMsgIdsFromDirectories {
+	my $filePath = $File::Find::name;
 
-    # Skip directories
-    return if -d $file_path;
-    return unless $file_path =~ /$start_directory(.*)\.po/;
+	# Skip directories
+	return if -d $filePath;
+	return unless $filePath =~ /startDirectory(.*)\.po/;
 
-    my $language_code = $1;
+	my $languageCode = $1;
 
-    open my $input_file, '<', $file_path or warn "Couldn't open $file_path: $!";
+	open my $inputFile, '<', $filePath or warn "Couldn't open $filePath: $!";
 
-    while (my $line = <$input_file>) {
-        if (my ($match) = $line =~ /msgid "(.*?)"/) {
-            if(length($match) > 0){
-                push @{$msgids{$language_code}}, $match;
-            }
-        }
-    }
+	while (my $line = <$inputFile>) {
+		if (my ($match) = $line =~ /msgid "(.*?)"/) {
+			if(length($match) > 0){
+				push @{$msgids{$languageCode}}, $match;
+			}
+		}
+	}
 
-    close $input_file;
+	close $inputFile;
 }
 
-# Perform the file search
-find({ wanted => \&load_msg_ids_from_directories, no_chdir => 1 }, $start_directory);
+# Subroutine to compare two lists
+sub compareLanguageWithTomteAndPrintDiff {
+	my ($languageCode) = @_;
 
-my %tomte_msgids;
+	print "Comparing Tomte Code msgids with those from $languageCode..\n";
+
+
+	my %unique_items = %{ dclone(\%tomteMsgids) };
+	my %returnHash;
+	my @onlyInList1;
+	my @onlyInList2;
+
+	# Check items in list 2
+	foreach my $item (@{$msgids{$language_code}}) {
+		if (exists $uniqueItems{$item}) {
+			delete $uniqueItems{$item};  # Remove common items
+		} else {
+			push @onlyInList2, $item;  # Item only in list 2
+		}
+	}
+
+	# Remaining items in %unique_items are only in list 1
+	@onlyInList1 = keys %uniqueItems;
+
+
+
+	my $list1Size = scalar @onlyInList1;
+	my $list2Size = scalar @onlyInList2;
+
+	print "Found $list1Size msgids only present in Tomte Code:\n";
+	if($list1Size > 0){
+		foreach my $msgid (@onlyInList1){
+			if (defined $msgid) {
+				print "$msgid\n";
+			} else {
+				print "Undefined value\n";  # Or handle it in another way
+			}
+		}
+
+		print "\n";
+	}
+
+	print "Found $list2Size msgids only present in $languageCode:\n";
+	if($list2Size > 0){
+		foreach my $msgid (@onlyInList2){
+			if (defined $msgid) {
+				print "$msgid\n";
+			} else {
+				print "Undefined value\n";  # Or handle it in another way
+			}
+		}
+
+		print "\n";
+	}
+	print "\n\n";
+
+	return $list1Size + $list2Size;
+}
+
+#sub readTomlFile {
+#	my $filePath = shift;
+#
+#	my $tomlStr = read_file($filePath);
+#	my $tomlHashRef = from_toml($toml_str);
+
+    # Check if the TOML data was parsed successfully
+	#    if (!defined $tomlHashRef) {
+	#        die("Failed to parse TOML data from file: $filePath");
+	#    }
+
+	#	return %$tomlHashRef;
+	#}
+
+
+# Perform the file search
+find({ wanted => \&loadMsgIdsFromDirectories, no_chdir => 1 }, startDirectory);
+
+my %tomteMsgids;
 
 # Open the tomte file
-open my $tomte_file, '<', 'src/tuxedo-tomte' or die "Couldn't open input file: $!";
+open my $tomteFile, '<', 'src/tuxedo-tomte' or die "Couldn't open input file: $!";
 
 # Process each line in the input file
-while (my $line = <$tomte_file>) {
-    # Find all text between __(' and ')
-    my @matches;
-    push @matches, $line =~ /__\((.*?)\)/g;
-    push @matches, $line =~ /__x\((.*?),/g;
-    push @matches, $line =~ /__n\((.*?),/g;
-    push @matches, $line =~ /__nx\((.*?),/g;
+while (my $line = <$tomteFile>) {
+	# Find all text between __(' and ')
+	my @matches;
+	push @matches, $line =~ /__\((.*?)\)/g;
+	push @matches, $line =~ /__x\((.*?),/g;
+	push @matches, $line =~ /__n\((.*?),/g;
+	push @matches, $line =~ /__nx\((.*?),/g;
 
-    # If at least one text was found, process them
-    if (@matches) {
-        foreach my $match (@matches) {
-            $match =~ s/^.(.*).$/$1/;  # Remove the first and last quotation marks
+	# If at least one text was found, process them
+	if (@matches) {
+		foreach my $match (@matches) {
+			$match =~ s/^.(.*).$/$1/;  # Remove the first and last quotation marks
 
-            # Check if this msgid is unique, then write it to the output
-            if (!$tomte_msgids{$match}) {
-                $tomte_msgids{$match} = 1;  # Mark as seen
-                # print "Tomte: $match\n";
-            }
-        }
-    }
+			# Check if this msgid is unique, then write it to the output
+			if (!$tomteMsgids{$match}) {
+				$tomteMsgids{$match} = 1;  # Mark as seen
+			# print "Tomte: $match\n";
+			}
+		}
+	}
 }
 
 # call descriptions are directly read from modules
-#delete $tomte_msgids{'module . "_description'};
-#my %origConfModules = read_toml_file('config/modules/modules_raw.toml');
+#delete $tomteMsgids{'module . "_description'};
+#my %origConfModules = readTomlFile('config/modules/modules_raw.toml');
 #foreach my $module (keys %origConfModules) {
 #    next if $module eq 'default';
 #    next if ! defined $module;
-#    my $module_string = $module . "_description";
-#    $tomte_msgids{$module_string} = 1;
+#    my $moduleString = $module . "_description";
+#    $tomteMsgids{$moduleString} = 1;
 #}
 
-# Subroutine to compare two lists
-sub compare_language_with_tomte_and_print_diff {
-    my ($language_code) = @_;
-
-     print "Comparing Tomte Code msgids with those from $language_code..\n";
-
-
-    my %unique_items = %{ dclone(\%tomte_msgids) };
-    my %return_hash;
-    my @only_in_list1;
-    my @only_in_list2;
-
-    # Check items in list 2
-    foreach my $item (@{$msgids{$language_code}}) {
-        if (exists $unique_items{$item}) {
-            delete $unique_items{$item};  # Remove common items
-        } else {
-            push @only_in_list2, $item;  # Item only in list 2
-        }
-    }
-
-    # Remaining items in %unique_items are only in list 1
-    @only_in_list1 = keys %unique_items;
-
-
-
-    my $list1_size = scalar @only_in_list1;
-    my $list2_size = scalar @only_in_list2;
-
-    print "Found $list1_size msgids only present in Tomte Code:\n";
-    if($list1_size > 0){
-        foreach my $msgid (@only_in_list1){
-            if (defined $msgid) {
-                print "$msgid\n";
-            } else {
-                print "Undefined value\n";  # Or handle it in another way
-            }
-        }
-
-        print "\n";
-    }
-
-    print "Found $list2_size msgids only present in $language_code:\n";
-    if($list2_size > 0){
-        foreach my $msgid (@only_in_list2){
-            if (defined $msgid) {
-                print "$msgid\n";
-            } else {
-                print "Undefined value\n";  # Or handle it in another way
-            }
-        }
-
-        print "\n";
-    }
-    print "\n\n";
-
-    return $list1_size + $list2_size;
-}
-
-#sub read_toml_file {
-#	my $file_path = shift;
-#
-#	my $toml_str = read_file($file_path);
-#	my $toml_hash_ref = from_toml($toml_str);
-
-    # Check if the TOML data was parsed successfully
-	#    if (!defined $toml_hash_ref) {
-	#        die("Failed to parse TOML data from file: $file_path");
-	#    }
-
-	#	return %$toml_hash_ref;
-	#}
 
 my $differences = 0;
-foreach my $language_code (keys %msgids){
-    $differences += compare_language_with_tomte_and_print_diff($language_code);
+foreach my $languageCode (keys %msgids){
+	$differences += compareLanguageWithTomteAndPrintDiff($languageCode);
 }
 
 exit $differences;
