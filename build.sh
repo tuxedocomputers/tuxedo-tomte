16944ddb873576b639d9a60d843b77a7ce6fd15f
emohrtuxedo <e.mohr@tuxedocomputers.com>
2024-07-03 13:45:59 +0200

    removed check

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-perl build_configurations.pl
-
-
 VERSIONSTRING=$(dpkg-parsechangelog | awk '/^Version:/ {print $$2}')
 sed -i "s|API-Change.Feature-Release.Hotfix-Release|$VERSIONSTRING|" src/tuxedo-tomte
 dpkg-buildpackage -uc -us -b
