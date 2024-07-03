361e212f479da55f0659ced71f12c0049d61fc1a
emohrtuxedo <e.mohr@tuxedocomputers.com>
2024-07-03 13:40:38 +0200

    removed some checks

--- check.sh
+++ check.sh
@@ -17,24 +17,24 @@ if [[ $higherloglevel_present -gt 0 ]]; then
 fi
 
 # Run the check_translations script and capture any output
-echo "Checking translations..."
-translation_differences_text=$(perl ./check_translations.pl 2>&1)
-translations_differences=$?
+#echo "Checking translations..."
+#translation_differences_text=$(perl ./check_translations.pl 2>&1)
+#translations_differences=$?
 
-if [[ $translations_differences -gt 0 ]]; then
-  echo "Found translations differences!"
-  echo "$translation_differences_text"
-fi
+#if [[ $translations_differences -gt 0 ]]; then
+#  echo "Found translations differences!"
+#  echo "$translation_differences_text"
+#fi
 
 
 # Check if there are syntax errors in the code
-syntax_incorrect=0
-echo "Checking for syntax errors..."
-perl -c ./src/tuxedo-tomte >/dev/null 2>&1 || syntax_incorrect=1
-if [[ $syntax_incorrect -gt 0 ]]; then
-  echo "Found syntax errors!"
-  perl -c ./src/tuxedo-tomte
-fi
+#syntax_incorrect=0
+#echo "Checking for syntax errors..."
+#perl -c ./src/tuxedo-tomte >/dev/null 2>&1 || syntax_incorrect=1
+#if [[ $syntax_incorrect -gt 0 ]]; then
+#  echo "Found syntax errors!"
+#  perl -c ./src/tuxedo-tomte
+#fi
 
 # Calculate the exit code as the sum of the variables
 exit_code=$((todo_present + higherloglevel_present + syntax_incorrect))
