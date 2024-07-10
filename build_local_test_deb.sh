e25e6673ceb899f792b0337662105852c2d9d749
emohrtuxedo <e.mohr@tuxedocomputers.com>
2024-07-10 13:14:38 +0200

    more debug

--- build_local_test_deb.sh
+++ build_local_test_deb.sh
@@ -41,9 +41,9 @@ TIMESTAMP=$(git log -1 --format="%at" | xargs -I{} date -d @{} +"%Y%m%d-%H%M%S")
 
 
 
-#TEST_VERSION="$BASE_VERSION-test-$TIMESTAMP-$BRANCH_SHORT"
+TEST_VERSION="$BASE_VERSION-test-$TIMESTAMP-$BRANCH_SHORT"
 #TEST_VERSION="$BASE_VERSION"
-TEST_VERSION="2.35.2-bionic"
+#TEST_VERSION="2.35.2-bionic"
 
 
 
